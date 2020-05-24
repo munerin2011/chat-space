ea5fad4ea5ebba4fa8ca2f1ee1d4ceec29274c09
munerin2011 <sum.suzu0924@gmail.com>
2020-05-24 15:21:33 +0900

    fixed_message.js
    重複記述の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,10 +41,11 @@ $(function(){
       $('.message-list').append(html);
       $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight });
       $('form')[0].reset();
-      $('.submit-btn').attr('disabled', false);
     })
     .fail(function(){
       alert('メッセージ送信に失敗しました')
+    })
+    .always(function(){
       $('.submit-btn').attr('disabled', false);
     })
   })
