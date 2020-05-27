9ba744ba9cf947ee504faa612d3c5bbcf4922572
munerin2011 <sum.suzu0924@gmail.com>
2020-05-27 13:51:21 +0900

    edit-automatic_update
    # What
    ChatSpaceの自動更新機能実装
    
    # Why
    現状、別のユーザーの投稿メッセージはリロードしなければ表示されず、アプリケーションの最低限の機能として自動でチャット画面が更新されるようにすることが必要であるため、自動更新機能を実装した。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,11 @@
 $(function(){
   function buildHTML(message){
     if (message.image) {
-      var current_user = `<img src=${message.image}>`
+      var imageSet = `<img src=${message.image}>`
     } else {
-      var current_user = ''
+      var imageSet = ''
     }
-    var html = `<div class="post">
+    var html = `<div class="post" data-post-id=${message.id}>
                   <div class="post__info">
                     <div class="post__info__name">
                       ${message.user_name}
@@ -18,7 +18,7 @@ $(function(){
                     <p class="post__comment__body">
                       ${message.body}
                     </p>
-                    ${current_user}
+                    ${imageSet}
                   </div>
                 </div>`
     return html
@@ -49,4 +49,30 @@ $(function(){
       $('.submit-btn').attr('disabled', false);
     })
   })
+
+  var reloadMessages = function(){
+    var last_message_id = $('.post:last').data('post-id');
+    $.ajax({
+      url: 'api/messages',
+      type: 'get',
+      data: {id: last_message_id},
+      dataType: 'json'
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+        insertHTML += buildHTML(message)
+        });
+        $('.message-list').append(insertHTML)
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight });
+      }
+    })
+    .fail(function(){
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  }
 });
