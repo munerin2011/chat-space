fb32423adcaff3c023e26c8b46325473b4df68e5
munerin2011 <sum.suzu0924@gmail.com>
2020-05-31 17:37:34 +0900

    edit-S3_upload_local
    ローカルのChatSpaceの画像保存先をS3に変更
    ・S3のバケット（データの保存場所）を用意する
    ・ChatSpaceの設定を変更し、保存先がS3になるようにする

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,13 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # minimagickを使うため
   include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   #サムネイルの為に画像をリサイズ
   process resize_to_fit: [200, 200]
+
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
